2c29791eba1dbc3ade9954fcfc9a94ca355f6bf1
Kim Hojeong <hojeong0713@gmail.com>
2021-06-08 22:28:31 +0200

    Fetch data

--- my-new-app/src/pages/NewMeetup.js
+++ my-new-app/src/pages/NewMeetup.js
@@ -1,10 +1,29 @@
+import { useHistory } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetup(props) {
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-9322e-default-rtdb.europe-west1.firebasedatabase.app/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
