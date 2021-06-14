0b1952ccffbf213690339fb925564ae00fc7e523
Kim Hojeong <hojeong0713@gmail.com>
2021-06-14 15:26:32 +0200

    Add expense list

---
+++ react-complete-guide/src/components/Expenses.js
@@ -0,0 +1,21 @@
+import Card from "./Card";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(props) {
+  return (
+    <Card className="expenses">
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          id={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </Card>
+  );
+}
+
+export default Expenses;
